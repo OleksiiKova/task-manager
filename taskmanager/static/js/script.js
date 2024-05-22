79d980b0b6e011d6fc69de1c2ab2c079b9746182
Oleksii Kovalenko <a16kova@gmail.com>
2024-05-22 12:28:19 +0100

    Ad Create tasks (CRUD functionality)

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Sidenav initialization 
     var sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
-  });
+
+    // Datepicker initialization 
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // Select initialization 
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+});
